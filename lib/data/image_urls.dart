7fbb9963a6b5e73dca003124d2ed5e7bd4b6549e
Ilya Evseev <ilyaevseev98@bk.ru>
2024-05-30 20:17:36 +0300

    initial commit

---
+++ lib/data/image_urls.dart
@@ -0,0 +1,28 @@
+const String m1 =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg';
+
+const String m2 =
+  'https://www.gourmet-magazin.de/fileadmin/user_upload/rezepte/gerichte/vorspeise-snack/toast-hawaii-5.jpg';
+
+const String m3 =
+  'https://tasteandflavors.com/wp-content/uploads/2020/05/CLASSIC-BURGER.jpg';
+
+const String m4 =
+  'https://gas-kvas.com/uploads/posts/2023-08/1691402903_gas-kvas-com-p-risunki-na-prazdnik-den-venskogo-shnitsely-19.jpg';
+
+const String m5 =
+  'https://therectangular.com/wp-content/uploads/2020/10/salad-with-smoked-salmon-inspirational-smoked-salmon-salad-with-cranberries -of-salad-with-smoked-salmon.jpg';
+
+const String m6 = 'https://i.ytimg.com/vi/ba_3ICebDoo/maxresdefault.jpg';
+
+const String m7 =
+  'https://kiddipedia.com.au/wp-content/uploads/2018/02/Depositphotos_63091657_m-2015.jpg';
+
+const String m8 =
+  'https://www.awarenessdays.com/wp-content/uploads/2021/10/iStock-1227594550-1-1536x1024.jpg';
+
+const String m9 =
+  'https://i.pinimg.com/originals/93/b8/d2/93b8d27c671aea783befb86df60d38cf.jpg';
+
+const String m10 =
+  'https://thumbs.dreamstime.com/b/vegan-warm-salad-roasted-asparagus-vegan-warm-salad-roasted-asparagus-tomatoes-99061113.jpg';
---
+++ lib/screens/meals.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meal_details.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    super.key,
+    this.title,
+    required this.meals,
+  });
+
+  final String? title;
+  final List<Meal> meals;
+
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(
+          meal: meal,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Разрабы ничего не добавили, сорян... o_0',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(height: 24),
+          Text(
+            'Поищи рецепты в другой категории 0_о',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(
+          meal: meals[index],
+          onSelectMeal: (meal) {
+            selectMeal(context, meal);
+          },
+        ),
+      );
+    }
+
+    if (title == null) {
+      return content;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title!),
+      ),
+      body: content,
+    );
+  }
+}
